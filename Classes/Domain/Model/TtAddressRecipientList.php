d3c702690c58d8542671196533753e60b01a4565
Eike Starkmann <eike.starkmann@posteo.de>
2023-02-22 16:46:54 +0100

    [TASK] use correct Repository

--- Classes/Domain/Model/TtAddressRecipientList.php
+++ Classes/Domain/Model/TtAddressRecipientList.php
@@ -7,7 +7,6 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use Undkonsorten\CuteMailing\Domain\Model\RecipientList;
 use Undkonsorten\CuteMailing\Domain\Model\RecipientListInterface;
-use Undkonsorten\CuteMailingRegisteraddress\Domain\Repository\RegisteraddressRecipientRepository;
 use Undkonsorten\CuteMailingTtAddress\Domain\Repository\TtAddressRecipientRepository;
 
 class TtAddressRecipientList extends RecipientList implements RecipientListInterface
@@ -37,7 +36,7 @@ class TtAddressRecipientList extends RecipientList implements RecipientListInter
     public function getRecipientsCount(): int
     {
         /**@var $addressRepository AddressRepository * */
-        $addressRepository = GeneralUtility::makeInstance(RegisteraddressRecipientRepository::class);
+        $addressRepository = GeneralUtility::makeInstance(TtAddressRecipientRepository::class);
         /**@var $defaultQuerySettings Typo3QuerySettings* */
         $defaultQuerySettings = $this->defaultQuerySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
         $defaultQuerySettings->setRespectStoragePage(true);
