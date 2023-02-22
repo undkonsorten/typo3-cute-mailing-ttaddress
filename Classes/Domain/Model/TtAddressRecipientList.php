e6ad766db574430dd67fe86c78f35391edae2972
Eike Starkmann <eike.starkmann@posteo.de>
2023-02-22 13:47:14 +0100

    [TASK] adapt interface changes

--- Classes/Domain/Model/TtAddressRecipientList.php
+++ Classes/Domain/Model/TtAddressRecipientList.php
@@ -2,10 +2,12 @@
 
 namespace Undkonsorten\CuteMailingTtAddress\Domain\Model;
 
+use FriendsOfTYPO3\TtAddress\Domain\Repository\AddressRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use Undkonsorten\CuteMailing\Domain\Model\RecipientList;
 use Undkonsorten\CuteMailing\Domain\Model\RecipientListInterface;
+use Undkonsorten\CuteMailingRegisteraddress\Domain\Repository\RegisteraddressRecipientRepository;
 use Undkonsorten\CuteMailingTtAddress\Domain\Repository\TtAddressRecipientRepository;
 
 class TtAddressRecipientList extends RecipientList implements RecipientListInterface
@@ -14,7 +16,7 @@ class TtAddressRecipientList extends RecipientList implements RecipientListInter
     /**
      * @inheritDoc
      */
-    public function getRecipients(): array
+    public function getRecipients(int $limit = null, int $offset = null): array
     {
         $result = [];
         /**@var $addressRepository TtAddressRecipientRepository * */
@@ -24,9 +26,24 @@ class TtAddressRecipientList extends RecipientList implements RecipientListInter
         $defaultQuerySettings->setRespectStoragePage(true);
         $defaultQuerySettings->setStoragePageIds([$this->getRecipientListPage()]);
         $addressRepository->setDefaultQuerySettings($defaultQuerySettings);
-        $result = $addressRepository->findAll()->toArray();
+        $result = $addressRepository->findAll($limit, $offset)->toArray();
 
         return $result;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRecipientsCount(): int
+    {
+        /**@var $addressRepository AddressRepository * */
+        $addressRepository = GeneralUtility::makeInstance(RegisteraddressRecipientRepository::class);
+        /**@var $defaultQuerySettings Typo3QuerySettings* */
+        $defaultQuerySettings = $this->defaultQuerySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $defaultQuerySettings->setRespectStoragePage(true);
+        $defaultQuerySettings->setStoragePageIds([$this->getRecipientListPage()]);
+        $addressRepository->setDefaultQuerySettings($defaultQuerySettings);
+        return $addressRepository->findAll()->count();
     }
 
     /**
