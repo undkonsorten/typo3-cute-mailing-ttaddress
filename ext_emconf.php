8232f5d79c3ad3009a51819fabfc7c3cb86d71d8
Eike Starkmann <eike.starkmann@posteo.de>
2023-03-10 10:36:06 +0100

    [TASK] bump version number to 2.0.0 and adjust dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,11 +12,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'cute_mailing' => '2.0.0-2.99.99',
+            'cute_mailing' => '3.0.0-3.99.99',
             'tt_address' => '5.2.0-7.99.99',
         ],
     ],
