8424223c980fe2202759f1fc2dc71fa2979d05e2
Eike Starkmann <eike.starkmann@posteo.de>
2023-06-28 18:05:07 +0200

    [TASK] bump version number to 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'undkonsorten',
     'author_email' => 'kontakt@undkonsorten.com',
     'state' => 'stable',
-    'version' => '2.0.0',
+    'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.9.99',
             'cute_mailing' => '3.0.0-3.99.99',
             'tt_address' => '5.2.0-7.99.99',
         ],
